be3ec3b49f3adf14e3e27eaafe365f7dcadcfc68
Mukku <mukkuraj0499@gmail.com>
2024-01-12 00:53:10 +0530

    Update code

---
+++ React-Classes/12megaBlog/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <>
+     <img className='w-20 border-4 border-pink-600 rounded-full animate-pulse' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBC2IMy1DmGzUgrtmwGf6XKdlz9iWgPObiFg&usqp=CAU"/>
+    
+    </>
+    
+  )
+}
+
+export default Logo
